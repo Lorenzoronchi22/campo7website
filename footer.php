5d3f4a97b6cfa31fb9add59dee14c3c33cffc876
Lorenzoronchi22 <lolloronchi98@gmail.com>
2022-11-28 18:24:22 +0100

    modifiche footer home aggiunta cartella CSS

--- footer.php
+++ footer.php
@@ -1,31 +1,20 @@
 <section class="footer">
 
-   <div class="box-container">
+   <div class="">
 
-      <div class="box">
-         <h3>Link</h3>
-         <a href="home.php">home</a>
-         <a href="about.php">about</a>
-         <a href="shop.php">shop</a>
-         <a href="contact.php">contact</a>
-      </div>
-
-      <div class="box">
+      <div class="">
          <h3>Per informazioni:</h3>
-         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
-         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
-         <p> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </p>
-         <p> <i class="fas fa-map-marker-alt"></i> mumbai, india - 400104 </p>
+         <p> <i class=""></i> 0306830108 </p>
+         <p> <i class=""></i> info@polisportivacaino.it </p>
+         <p> <i class=""></i> Caino, Italia - 25070 </p>
       </div>
 
       <div class="box">
          <h3>Seguici su:</h3>
-         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="https://www.facebook.com/polisportivacaino.it"> <i class=""></i> Facebook </a>
          <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
       </div>
 
    </div>
-
-   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>mr. web designer</span> </p>
 
 </section>
